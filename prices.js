b61bbe4c960c8397ed00cf2b0d15377a5c6182a5
marcellgajdos <gajdosmarcell.gm@gmail.com>
2024-12-18 14:16:24 +0100

    initial commit

---
+++ prices.js
@@ -0,0 +1,214 @@
+﻿var meznev1="Akácméz"
+var leiras1="Kellemes, enyhe ízvilág. Teába, kávéba, sütéshez, kenyérre ajánlott. Kevésbé savas kémhatású. Magas fruktóz tartalma miatt lassan kristályosodó méz."
+var mez1="900 g: 3400 Ft"
+var mez1_1="500 g: 1900 Ft"
+var mez1_2="250 g: 1100 Ft"
+
+var meznev2="Erdei méz"
+var leiras2="Különleges ízvilág. Édes íz, de eltér a megszokottól. Magas ásványianyag tartalommal rendelkezik. Döntően nem nektárból van, hanem a tetvek által megszúrt levelek (leggyakrabban fenyő, továbbá lombos fák) édes váladékából. Erdei vegyesméz (mézharmat). Magában, csemegeként ajánlott fogyasztani."
+var mez2="900 g: 3900 Ft"
+var mez2_1="500 g: 2100 Ft"
+var mez2_2="250 g: 1200 Ft"
+
+var meznev3="Facéliaméz"
+var leiras3="Más néven mézontófű. Íze semlegesebb, hasonlít az akácéra. Állaga sűrű, könnyen kanalazható. Magas fruktóz tartalma révén lassabban kristályosodik. A növény egy szívgyógyszer alapanyaga, ebből kifolyólag a méz is a szívre és az érrendszerre gyakorolt jótékony hatásairól ismert."
+var mez3="nem elérhető"
+var mez3_1="<br>"
+var mez3_2="<br>"
+
+var meznev4="Gesztenyeméz"
+var leiras4="Különleges, kesernyés ízvilág. Magas ásványianyag tartalommal rendlekezik. Jótékony hatással van az érrendszerre. Fogyasztása magában ajánlott, különlegessége révén."
+var mez4="nem elérhető"
+var mez4_1="<br>"
+var mez4_2="<br>"
+
+var meznev5="Hársméz"
+var leiras5="A intenzív, zamatos, karakteres ízvilág. Felső légúti panaszok (pl. köhögés) enyhítésére, immunerősítésre alkalmas. Teába, kávéba ajánlott."
+var mez5="900 g: 3100 Ft"
+var mez5_1="500 g: 1700 Ft"
+var mez5_2="250 g: 1000 Ft"
+
+var meznev6="Napraforgóméz"
+var leiras6="Jellegzetes ízű, krémes állagú. Savas kémhatású. Tökéletes helyettesítője a kristálycukornak. Teába, kávéba, sütéshez ajánlott. Gyulladáscsökkentő hatású."
+var mez6="900 g: 2100 Ft"
+var mez6_1="<br>"
+var mez6_2="<br>"
+
+var meznev7="Repce krémméz"
+var leiras7="Semlegesebb ízű, krémes állagú. Inkább lúgos kémhatású méz, gyomorbántalmak/savtúltengés esetén is ehető. Magas glükóz tartalma miatt kristályosodik. Magas vastartalommal rendelkezik. Semleges ízének köszönhetően bármilyen ételbe, italba használható édesítőként. Cukorbetegeknek ajánlott, mivel a 11 cukor közül csak fruktózt, glükózt és maltózt tartalmaz."
+var mez7="900 g: 2300 Ft"
+var mez7_1="500 g: 1300 Ft"
+var mez7_2="<br>"
+
+var meznev8="Selyemfűméz"
+var leiras8="Más néven vaddohány. Kellemes ízvilág, finom utóíz. Teába, kávéba egyaránt kiváló. A növénynek nagyon alacsony a pollentartalma, így a méz szinte pollen mentes. Ennek köszönhetően a mézet az allergiások is fogyaszthatják."
+var mez8="900 g: 3100 Ft"
+var mez8_1="500 g: 1700 Ft"
+var mez8_2="250 g: 1000 Ft"
+
+var meznev9="Vegyes virágméz"
+var leiras9="Nyári vegyes méz, finom ízvilág. Teába, kávéba, sütéshez, megfázáskor ajánlott."
+var mez9="900 g: 2300 Ft"
+var mez9_1="500 g: 1300 Ft"
+var mez9_2="<br>"
+
+var meznev10="Maciméz 250 g"
+var leiras10=""
+var mez10="Erdei: 1200 Ft"
+var mez10_1="Akác, hárs, selyemfű: 1100 Ft"
+var mez10_2="Vegyes: 900 Ft"
+
+var meznev11="Propolisz tinktúra"
+var leiras11="A legkiválóbb immunerősítő, gyulladáscsökkentő, baktérium és vírusölő hatású. Naponta 5-10 csepp ajánlott. Betegség esetén naponta többször is lehet alkalmazni. Magában is fogyasztható, azonban keserű íze miatt általában egy kiskanál mézre/kristálycukorra csepegtetve finomabb. Külsőleg is alkalmazható, sebekre, kiütésre, láb/körömgombára. Afta esetén is kiválóan hat,  összehúzza a sebet. Ez esetben vattára csepegtetve ecseteljük be a sebet."
+var mez11="30 ml: 1700 Ft"
+var mez11_1="<br>"
+var mez11_2="<br>"
+
+var meznev12="Szárított virágpor"
+var leiras12="Vitaminbomba, melynek fogyasztása serkenti az immunrendszert és hozzájárulhat az egészséges emésztéshez. Ezenkívül természetes energia- és fehérjeforrás."
+var mez12="450 g: 3300 Ft"
+var mez12_1="230 g: 1900 Ft"
+var mez12_2="130 g: 1200 Ft"
+
+var meznev13="Pumpa"
+var leiras13="Üvegre szerelhető pumpa a kényelmes adagoláshoz. 1, 0,5 és 250 g-os üvegekre. Használat során hetente meleg vízzel átöblítendő!"
+var mez13="800 Ft"
+var mez13_1="<br>"
+var mez13_2="<br>"
+
+
+document.getElementById("meznev1").innerHTML=meznev1; //azert van elol ez a blokk, hogy az index.html-en levo elemek mukodjenek (sorban fut, ha nincs olyan id, megakad)
+document.getElementById("meznev2").innerHTML=meznev2;
+document.getElementById("meznev3").innerHTML=meznev3;
+document.getElementById("meznev4").innerHTML=meznev4;
+document.getElementById("m1").innerHTML=mez1;
+document.getElementById("m1_1").innerHTML=mez1_1;
+document.getElementById("m1_2").innerHTML=mez1_2;
+document.getElementById("m2").innerHTML=mez2;
+document.getElementById("m2_1").innerHTML=mez2_1;
+document.getElementById("m2_2").innerHTML=mez2_2;
+document.getElementById("m3").innerHTML=mez3;
+document.getElementById("m3_1").innerHTML=mez3_1;
+document.getElementById("m3_2").innerHTML=mez3_2;
+document.getElementById("m4").innerHTML=mez4;
+document.getElementById("m4_1").innerHTML=mez4_1;
+document.getElementById("m4_2").innerHTML=mez4_2;
+
+document.getElementById("em1").innerHTML=mez1;
+document.getElementById("em1_1").innerHTML=mez1_1;
+document.getElementById("em1_2").innerHTML=mez1_2;
+
+document.getElementById("emeznev1").innerHTML=meznev1;
+document.getElementById("leiras1").innerHTML=leiras1;
+
+document.getElementById("em2").innerHTML=mez2;
+document.getElementById("em2_1").innerHTML=mez2_1;
+document.getElementById("em2_2").innerHTML=mez2_2;
+
+document.getElementById("emeznev2").innerHTML=meznev2;
+document.getElementById("leiras2").innerHTML=leiras2;
+
+document.getElementById("em3").innerHTML=mez3;
+document.getElementById("em3_1").innerHTML=mez3_1;
+document.getElementById("em3_2").innerHTML=mez3_2;
+
+document.getElementById("emeznev3").innerHTML=meznev3;
+document.getElementById("leiras3").innerHTML=leiras3;
+
+document.getElementById("em4").innerHTML=mez4;
+document.getElementById("em4_1").innerHTML=mez4_1;
+document.getElementById("em4_2").innerHTML=mez4_2;
+
+document.getElementById("emeznev4").innerHTML=meznev4;
+document.getElementById("leiras4").innerHTML=leiras4;
+
+document.getElementById("m5").innerHTML=mez5;
+document.getElementById("m5_1").innerHTML=mez5_1;
+document.getElementById("m5_2").innerHTML=mez5_2;
+document.getElementById("em5").innerHTML=mez5;
+document.getElementById("em5_1").innerHTML=mez5_1;
+document.getElementById("em5_2").innerHTML=mez5_2;
+document.getElementById("meznev5").innerHTML=meznev5;
+document.getElementById("emeznev5").innerHTML=meznev5;
+document.getElementById("leiras5").innerHTML=leiras5;
+
+document.getElementById("m6").innerHTML=mez6;
+document.getElementById("m6_1").innerHTML=mez6_1;
+document.getElementById("m6_2").innerHTML=mez6_2;
+document.getElementById("em6").innerHTML=mez6;
+document.getElementById("em6_1").innerHTML=mez6_1;
+document.getElementById("em6_2").innerHTML=mez6_2;
+document.getElementById("meznev6").innerHTML=meznev6;
+document.getElementById("emeznev6").innerHTML=meznev6;
+document.getElementById("leiras6").innerHTML=leiras6;
+
+document.getElementById("m7").innerHTML=mez7;
+document.getElementById("m7_1").innerHTML=mez7_1;
+document.getElementById("m7_2").innerHTML=mez7_2;
+document.getElementById("em7").innerHTML=mez7;
+document.getElementById("em7_1").innerHTML=mez7_1;
+document.getElementById("em7_2").innerHTML=mez7_2;
+document.getElementById("meznev7").innerHTML=meznev7;
+document.getElementById("emeznev7").innerHTML=meznev7;
+document.getElementById("leiras7").innerHTML=leiras7;
+
+document.getElementById("m8").innerHTML=mez8;
+document.getElementById("m8_1").innerHTML=mez8_1;
+document.getElementById("m8_2").innerHTML=mez8_2;
+document.getElementById("em8").innerHTML=mez8;
+document.getElementById("em8_1").innerHTML=mez8_1;
+document.getElementById("em8_2").innerHTML=mez8_2;
+document.getElementById("meznev8").innerHTML=meznev8;
+document.getElementById("emeznev8").innerHTML=meznev8;
+document.getElementById("leiras8").innerHTML=leiras8;
+
+document.getElementById("m9").innerHTML=mez9;
+document.getElementById("m9_1").innerHTML=mez9_1;
+document.getElementById("m9_2").innerHTML=mez9_2;
+document.getElementById("em9").innerHTML=mez9;
+document.getElementById("em9_1").innerHTML=mez9_1;
+document.getElementById("em9_2").innerHTML=mez9_2;
+document.getElementById("meznev9").innerHTML=meznev9;
+document.getElementById("emeznev9").innerHTML=meznev9;
+document.getElementById("leiras9").innerHTML=leiras9;
+
+document.getElementById("m10").innerHTML=mez10;
+document.getElementById("m10_1").innerHTML=mez10_1;
+document.getElementById("m10_2").innerHTML=mez10_2;
+document.getElementById("em10").innerHTML=mez10;
+document.getElementById("em10_1").innerHTML=mez10_1;
+document.getElementById("em10_2").innerHTML=mez10_2;
+document.getElementById("meznev10").innerHTML=meznev10;
+document.getElementById("emeznev10").innerHTML=meznev10;
+document.getElementById("leiras10").innerHTML=leiras10;
+
+document.getElementById("m11").innerHTML=mez11;
+document.getElementById("m11_1").innerHTML=mez11_1;
+document.getElementById("m11_2").innerHTML=mez11_2;
+document.getElementById("em11").innerHTML=mez11;
+document.getElementById("em11_1").innerHTML=mez11_1;
+document.getElementById("em11_2").innerHTML=mez11_2;
+document.getElementById("meznev11").innerHTML=meznev11;
+document.getElementById("emeznev11").innerHTML=meznev11;
+document.getElementById("leiras11").innerHTML=leiras11;
+
+document.getElementById("m12").innerHTML=mez12;
+document.getElementById("m12_1").innerHTML=mez12_1;
+document.getElementById("m12_2").innerHTML=mez12_2;
+document.getElementById("em12").innerHTML=mez12;
+document.getElementById("em12_1").innerHTML=mez12_1;
+document.getElementById("em12_2").innerHTML=mez12_2;
+document.getElementById("meznev12").innerHTML=meznev12;
+document.getElementById("emeznev12").innerHTML=meznev12;
+document.getElementById("leiras12").innerHTML=leiras12;
+
+document.getElementById("m13").innerHTML=mez13;
+document.getElementById("m13_1").innerHTML=mez13_1;
+document.getElementById("m13_2").innerHTML=mez13_2;
+document.getElementById("em13").innerHTML=mez13;
+document.getElementById("em13_1").innerHTML=mez13_1;
+document.getElementById("em13_2").innerHTML=mez13_2;
+document.getElementById("meznev13").innerHTML=meznev13;
+document.getElementById("emeznev13").innerHTML=meznev13;
+document.getElementById("leiras13").innerHTML=leiras13;
+
